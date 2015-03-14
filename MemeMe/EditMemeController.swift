9ef5c69f48685b2fd14b5eaced848fc00a666bbf
ivan lares <ivanlares@ivans-MacBook-Pro.local>
2015-03-14 15:14:36 -0700

    I added EditMeme and Table Controller(s)

---
+++ MemeMe/EditMemeController.swift
@@ -0,0 +1,24 @@
+//
+//  EditMemeController.swift
+//  MemeMe
+//
+//  Created by ivan lares on 3/14/15.
+//  Copyright (c) 2015 Ivan Lares. All rights reserved.
+//
+
+import UIKit
+
+class EditMemeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+}
